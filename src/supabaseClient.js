c52798064cc001603de84556b72b438f8646d10f
Zach Walters <zwsullybuttes@gmail.com>
2026-03-01 21:32:38 -0700

    hardcode supabase config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl  = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey  = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://yelicgqkqerpmmmifhewn.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllbGljZ3FrcWVycG1taWZoZXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTcxMzQsImV4cCI6MjA4Nzc5MzEzNH0.nlb0kirN0wnZTxfFCYW1Jz7db0ggA6rRBW1L0LZglQ0'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+```
+
+Save, then:
+```
+git add src/supabaseClient.js
+git commit -m "hardcode supabase config"
+git push
