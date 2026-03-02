e6375a555fbcbfea17fb9aef91b27c78014e06ab
Zach Walters <zwsullybuttes@gmail.com>
2026-03-01 21:36:45 -0700

    fix supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,13 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://yelicgqkqerpmmmifhewn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllbGljZ3FrcWVycG1taWZoZXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTcxMzQsImV4cCI6MjA4Nzc5MzEzNH0.nlb0kirN0wnZTxfFCYW1Jz7db0ggA6rRBW1L0LZglQ0'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-```
-
-Save, then:
-```
-git add src/supabaseClient.js
-git commit -m "hardcode supabase config"
-git push
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://yelicgqkqerpmmmifhewn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllbGljZ3FrcWVycG1taWZoZXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTcxMzQsImV4cCI6MjA4Nzc5MzEzNH0.nlb0kirN0wnZTxfFCYW1Jz7db0ggA6rRBW1L0LZglQ0';
+export const supabase = createClient(supabaseUrl, supabaseKey);
