2d1249b87e0e53d5306c6c242fca8b81f545b71c
Zach Walters <zwsullybuttes@gmail.com>
2026-03-01 21:47:44 -0700

    use publishable key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://yelicgqkerpmmifhewn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllbGljZ3FrcWVycG1taWZoZXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMTcxMzQsImV4cCI6MjA4Nzc5MzEzNH0.nlb0kirN0wnZTxfFCYW1Jz7db0ggA6rRBW1L0LZglQ0';
+const supabaseKey = 'sb_publishable_8brgEBU1zPSCdl6oVXu6Bg_IHMHwShi';
 export const supabase = createClient(supabaseUrl, supabaseKey);
